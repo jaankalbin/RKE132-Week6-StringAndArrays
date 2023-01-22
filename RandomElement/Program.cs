d37c829a20892ddff9760fb17444e082b06f767b
Jaan Kalbin <jaan.kalbin@tktk.ee>
2023-01-22 23:05:38 +0200

    add Week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "Asian takeaway" };
+Random rnd = new Random();
+int randomIndex = rnd.Next(0, snacks.Length);
+Console.WriteLine($"Tonight, we are going to eat {snacks[randomIndex]}.");
